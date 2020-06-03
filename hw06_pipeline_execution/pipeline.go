18ad4a467b440236d797d177ca25c203bcbdbb84
Roman Zatonskiy <roman.zatonskiy@iqoption.com>
2020-06-04 02:55:12 +0300

    hm06: added pipeline logic and tests

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,48 @@
 package hw06_pipeline_execution //nolint:golint,stylecheck
 
 type (
-	In  = <-chan interface{}
+	I   = interface{}
+	In  = <-chan I
 	Out = In
-	Bi  = chan interface{}
 )
 
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	var prevOut = in
+	for _, stage := range stages {
+		if stage != nil {
+			prevOut = createPipe(stage, prevOut, done)
+		}
+	}
+
+	return prevOut
+}
+
+func createPipe(stage Stage, in In, done In) Out {
+	var out = make(chan I, cap(in))
+
+	go func() {
+		defer close(out)
+
+		if done == nil {
+			for item := range stage(in) {
+				out <- item
+			}
+		} else {
+			for {
+				select {
+				case item, ok := <-stage(in):
+					if !ok {
+						return
+					}
+					out <- item
+				case <-done:
+					return
+				}
+			}
+		}
+	}()
+
+	return out
 }
