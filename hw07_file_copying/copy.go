72d2d354eb93fb572cff2343a821507d8b1a5a8c
Roman Zatonskiy <roman.zatonskiy@iqoption.com>
2020-06-14 00:22:08 +0300

    hm07: added logic copying files

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,192 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrEmptyPath             = errors.New("empty file path")
+	//ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidArgument       = os.ErrInvalid
 )
 
+type validator func() error
+
+type copyArgs struct {
+	fromPath string
+	toPath   string
+	offset   int64
+	limit    int64
+}
+
+type fileOperation func(sourceFile *os.File, args copyArgs) error
+
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+	var args = &copyArgs{
+		fromPath: fromPath,
+		toPath:   toPath,
+		offset:   offset,
+		limit:    limit,
+	}
+
+	// validate
+	if err := validateArgs(*args); err != nil {
+		return err
+	}
+
+	// open
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	var operations = []fileOperation{
+		seek,
+		copying,
+	}
+
+	for _, action := range operations {
+		if err := action(sourceFile, *args); err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func seek(sourceFile *os.File, args copyArgs) error {
+	fileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if args.offset > 0 {
+		if args.offset > fileInfo.Size() {
+			return ErrOffsetExceedsFileSize
+		}
+		// seek if need
+		if _, err := sourceFile.Seek(offset, io.SeekStart); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copying(sourceFile *os.File, args copyArgs) error {
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+	// create temp file
+	tempDstFile, err := ioutil.TempFile("./", "out_*")
+	if err != nil {
+		return err
+	}
+
+	// calc max qty bytes for transfer
+	var transferLimit = sourceFileInfo.Size()
+	if limit > 0 && limit < sourceFileInfo.Size() {
+		transferLimit = limit
+	}
+
+	var readBytes int64 = 0
+	var bufSize int64 = 8
+	bar := pb.Start64(transferLimit)
+	for readBytes <= transferLimit {
+		qty, err := io.CopyN(tempDstFile, sourceFile, bufSize)
+
+		readBytes += qty
+		bar.Add64(qty)
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Millisecond)
+	}
+	//bar.Finish()
+
+	if err := tempDstFile.Close(); err != nil {
+		return err
+	}
+
+	// finish copy
+	if err := os.Rename(tempDstFile.Name(), args.toPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateArgs(args copyArgs) error {
+
+
+	validators := []validator{
+		func() error { return isNotEmptyPath(args.fromPath) },
+		func() error { return isNotEmptyPath(args.toPath) },
+		func() error { return isPositiveNumber(args.offset) },
+		func() error { return isPositiveNumber(args.limit) },
+		// is dir
+		func() error {
+			sourceFile, err := os.Open(args.fromPath)
+			if err != nil {
+				return err
+			}
+			defer sourceFile.Close()
+
+			fileInfo, err := sourceFile.Stat()
+			if err != nil {
+				return err
+			}
+
+			if fileInfo.IsDir() {
+				return fmt.Errorf("%s is not a file", args.fromPath)
+			}
+
+			return nil
+		},
+	}
+	if err := validate(validators); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validate(validators []validator) error {
+	var err error
+	for _, v := range validators {
+		err = v()
+		if err != nil {
+			break
+		}
+	}
+
+	return err
+}
+
+func isNotEmptyPath(path string) error {
+	if path == "" {
+		return ErrEmptyPath
+	}
+
+	return nil
+}
+
+func isPositiveNumber(value int64) error {
+	if value < 0 {
+		return ErrInvalidArgument
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,123 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+
+	t.Run("simple", func(t *testing.T) {
+		var args = *&args{
+			fromPath: "testdata/input.txt",
+			toPath:   "out.txt",
+			offset:   int64(0),
+			limit:    int64(0),
+		}
+		expectFile, err := os.Open(args.fromPath)
+		assert.NoError(t, err)
+		defer expectFile.Close()
+
+		expectedFileInfo, err := expectFile.Stat()
+		assert.NoError(t, err)
+
+		result := Copy(args.fromPath, args.toPath, args.offset, args.limit)
+		assert.NoError(t, result)
+		assert.FileExists(t, args.toPath)
+
+		actualFile, err := os.Open(args.toPath)
+		assert.NoError(t, err)
+
+		actualFileInfo, err := actualFile.Stat()
+		assert.NoError(t, err)
+
+		assert.Equal(t, expectedFileInfo.Size(), actualFileInfo.Size())
+	})
+
+	validateCases := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "limit < 0", args: args{
+			fromPath: "testdata/input.txt",
+			toPath:   "out.txt",
+			offset:   int64(-2),
+			limit:    int64(0),
+		},
+			wantErr: ErrInvalidArgument,
+		},
+		{
+			name: "offset < 0", args: args{
+			fromPath: "testdata/input.txt",
+			toPath:   "out.txt",
+			offset:   int64(0),
+			limit:    int64(-1),
+		},
+			wantErr: ErrInvalidArgument,
+		},
+		{
+			name: "empty source path", args: args{
+			fromPath: "",
+			toPath:   "out.txt",
+			offset:   int64(0),
+			limit:    int64(0),
+		},
+			wantErr: ErrEmptyPath,
+		},
+		{
+			name: "empty dst path", args: args{
+			fromPath: "testdata/input.txt",
+			toPath:   "",
+			offset:   int64(0),
+			limit:    int64(0),
+		},
+			wantErr: ErrEmptyPath,
+		},
+		{
+			name: "offset more than source size", args: args{
+			fromPath: "testdata/input.txt",
+			toPath:   "out.txt",
+			offset:   int64(1024 * 1024),
+			limit:    int64(0),
+		},
+			wantErr: ErrOffsetExceedsFileSize,
+		},
+		{
+			name: "source path is directory", args: args{
+			fromPath: "testdata",
+			toPath:   "out.txt",
+			offset:   int64(0),
+			limit:    int64(0),
+		},
+			wantErr: errors.New("testdata is not a file"),
+		},
+	}
+	for _, tt := range validateCases {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit)
+			assert.Error(t, err)
+			assert.EqualError(t, err, tt.wantErr.Error())
+		})
+	}
+
+}
+
+func TestCopy1(t *testing.T) {
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+
 }
