bdf382d19163441a5c998e349e4e2f6f2670ed29
Roman Zatonskiy <roman.zatonskiy@iqoption.com>
2020-06-19 00:44:53 +0300

    hm08: added logic for hm08 and tests

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	type args struct {
+		cmd []string
+		env Environment
+	}
+
+	t.Run("when is ok", func(t *testing.T) {
+		var expectedCode int = 0
+		var args = args{
+			cmd: []string{
+				"ls",
+				"-la",
+			},
+			env: map[string]string{
+				"FOO": "BAR",
+			},
+		}
+
+		result := RunCmd(args.cmd, args.env)
+		assert.Equal(t, expectedCode, result)
+	})
+
+	t.Run("when empty command", func(t *testing.T) {
+		var expectedCode int = 1
+		var args = args{
+			env: map[string]string{
+				"FOO": "BAR",
+			},
+		}
+
+		result := RunCmd(args.cmd, args.env)
+		assert.Equal(t, expectedCode, result)
+	})
+
+	t.Run("when empty env", func(t *testing.T) {
+		var expectedCode int = 0
+		var args = args{
+			cmd: []string{
+				"ls",
+				"-la",
+			},
+		}
+
+		result := RunCmd(args.cmd, args.env)
+		assert.Equal(t, expectedCode, result)
+	})
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here
+	var args = os.Args
+	if len(args) <= 2 {
+		log.Fatalln("incorrect qty arguments. Please set by template: <environment dir> <command> [arguments] ")
+	}
+
+	var envDir = args[1]
+	dirEnvs, err := ReadDir(envDir)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	code := RunCmd(args[2:], dirEnvs)
+
+	os.Exit(code)
 }
